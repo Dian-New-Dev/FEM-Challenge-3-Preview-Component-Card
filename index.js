24667c9c111018ec58e57151f4a09641283d8922
Diego <diannewdev@gmail.com>
2023-12-10 22:49:49 -0400

    button acive state added, project finished

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const img = document.getElementById('top-img');
+
+window.addEventListener('load', function () {
+    if (window.innerWidth < 900) {
+        img.setAttribute('src', 'images/image-product-mobile.jpg' )
+    } else {
+        img.setAttribute('src', 'images/image-product-desktop.jpg')
+    }
+})
+
 window.addEventListener('resize', function () {
     if (window.innerWidth < 900) {
         img.setAttribute('src', 'images/image-product-mobile.jpg' )
@@ -6,3 +15,15 @@ window.addEventListener('resize', function () {
         img.setAttribute('src', 'images/image-product-desktop.jpg')
     }
 })
+
+const button = document.querySelector('button');
+button.addEventListener('mouseover', function () {
+    button.style.cursor = 'pointer';
+})
+button.addEventListener('mouseenter', function(){
+    button.classList.add('button-active');
+})
+button.addEventListener('mouseleave', function(){
+    button.classList.remove('button-active');
+})
+
